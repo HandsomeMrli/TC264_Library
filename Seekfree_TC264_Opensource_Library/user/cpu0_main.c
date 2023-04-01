b31901d6bef17ae552b579892a2ae1587d192c84
Yaner <1468275133@qq.com>
2023-04-01 20:30:37 +0800

    无线串口传输虚仿画面正常(摄像头上位机宽高应颠倒)

--- Seekfree_TC264_Opensource_Library/user/cpu0_main.c
+++ Seekfree_TC264_Opensource_Library/user/cpu0_main.c
@@ -74,7 +74,7 @@ fifo_struct uart_data_file;
 uint8 data_buffer[32];
 uint8 data_len;
 uint8 count = 0;    
-
+uint8 image_temp[MT9V03X_H][MT9V03X_W];
 
 int core0_main(void)
 {
@@ -83,6 +83,14 @@ int core0_main(void)
     // 此处编写用户代码 例如外设初始化代码等
 
 
+    if(wireless_uart_init()){
+        while(1){
+            ;
+        }
+    }
+    wireless_uart_send_byte('\r');
+    wireless_uart_send_byte('\n');
+    wireless_uart_send_string("SEEKFREE wireless uart demo.\r\n");   
     /*无线串口
     if(wireless_uart_init()){
         while(1){
@@ -101,35 +109,19 @@ int core0_main(void)
     int mode[8] = {0};
    */
 
-    gpio_init(LED_1_PIN, GPO, GPIO_LOW, GPO_PUSH_PULL);
-
-    gpio_init(BTN_1_PIN, GPI, GPIO_LOW, GPI_PULL_UP);
-    gpio_init(BTN_2_PIN, GPI, GPIO_LOW, GPI_PULL_UP);
-
-    gpio_init(SW_1_PIN, GPI, GPIO_LOW, GPI_PULL_UP);
-    gpio_init(SW_2_PIN, GPI, GPIO_LOW, GPI_PULL_UP);
-    gpio_init(SW_3_PIN, GPI, GPIO_LOW, GPI_PULL_UP);
-    gpio_init(SW_4_PIN, GPI, GPIO_LOW, GPI_PULL_UP);
-
-
-    fifo_init(&uart_data_file, FIFO_DATA_8BIT, uart_get_data, 64);
-    uart_init(UART_CHANNEL, 9600, UART2_TX_P10_5, UART2_RX_P10_6);
-    uart_rx_interrupt(UART_CHANNEL, 1);
-    uart_write_string(UART_CHANNEL, "UART init successful!");
-    uart_write_byte(UART_CHANNEL, '\r');
-    uart_write_byte(UART_CHANNEL, '\n');
-
-
-    tft180_init();
-    icm20602_init();
-    Init_MPU6050_GYRO();
-
     /* 方案二:在方案一的基础上,识别+360到+0之间的突变 */
     float yaw, rol, pitch;
     int8_t yawCount = 0, rolCount = 0, pitchCount = 0;
     float yawLast, rolLast, pitchLast;
     attitude_solution_func(icm20602_acc_x, icm20602_acc_y, icm20602_acc_z, icm20602_gyro_x, icm20602_gyro_y, icm20602_gyro_z, &yawLast, &rolLast, &pitchLast);
 
+
+
+    for(int i = 0; i < MT9V03X_H; ++i){
+        for(int j = 0; j < MT9V03X_W; ++j){
+            image_temp[i][j] = (i+j)%256;
+        }
+    }
 
     // 此处编写用户代码 例如外设初始化代码等
     cpu_wait_event_ready();         // 等待所有核心初始化完毕
@@ -138,13 +130,22 @@ int core0_main(void)
         // 此处编写需要循环执行的代码
 
 
+        data_len = (uint8)wireless_uart_read_buff(data_buffer, 32);             // 查看是否有消息 默认缓冲区是 WIRELESS_UART_BUFFER_SIZE 总共 64 字节
+        if(data_len != 0)                                                       // 收到了消息 读取函数会返回实际读取到的数据个数
+        {
+            wireless_uart_send_buff(data_buffer, data_len);                     // 将收到的消息发送回去
+            memset(data_buffer, 0, 32);
+            // func_uint_to_str((char *)data_buffer, data_len);
+        }
+        wireless_uart_send_image(image_temp[0], MT9V03X_IMAGE_SIZE);
+        system_delay_ms(1);
 	    /* 无线串口
         data_len = (uint8)wireless_uart_read_buff(data_buffer, 32);             // 查看是否有消息 默认缓冲区是 WIRELESS_UART_BUFFER_SIZE 总共 64 字节
         if(data_len != 0)                                                       // 收到了消息 读取函数会返回实际读取到的数据个数
         {
             wireless_uart_send_buff(data_buffer, data_len);                     // 将收到的消息发送回去
             memset(data_buffer, 0, 32);
-//            func_uint_to_str((char *)data_buffer, data_len);
+            // func_uint_to_str((char *)data_buffer, data_len);
         }
         system_delay_ms(1);
         */
@@ -169,38 +170,38 @@ int core0_main(void)
         mode[5] = gpio_get_level(BTN_2_PIN);
         */
 
-        icm20602_get_acc();
-        icm20602_get_gyro();
+        // icm20602_get_acc();
+        // icm20602_get_gyro();
 
-        tft180_show_string(0, 0, "accX");  
-        tft180_show_string(0, 16, "accY"); 
-        tft180_show_string(0, 32, "accZ"); 
-        tft180_show_string(0, 48, "gyroX");
-        tft180_show_string(0, 64, "gyroY");
-        tft180_show_string(0, 80, "gyroZ");
+        // tft180_show_string(0, 0, "accX");  
+        // tft180_show_string(0, 16, "accY"); 
+        // tft180_show_string(0, 32, "accZ"); 
+        // tft180_show_string(0, 48, "gyroX");
+        // tft180_show_string(0, 64, "gyroY");
+        // tft180_show_string(0, 80, "gyroZ");
 
-        tft180_show_int(44, 0, icm20602_acc_x, 6);  
-        tft180_show_int(44, 16, icm20602_acc_y, 6); 
-        tft180_show_int(44, 32, icm20602_acc_z, 6); 
-        tft180_show_int(44, 48, icm20602_gyro_x, 6);
-        tft180_show_int(44, 64, icm20602_gyro_y, 6);
-        tft180_show_int(44, 80, icm20602_gyro_z, 6);
+        // tft180_show_int(44, 0, icm20602_acc_x, 6);  
+        // tft180_show_int(44, 16, icm20602_acc_y, 6); 
+        // tft180_show_int(44, 32, icm20602_acc_z, 6); 
+        // tft180_show_int(44, 48, icm20602_gyro_x, 6);
+        // tft180_show_int(44, 64, icm20602_gyro_y, 6);
+        // tft180_show_int(44, 80, icm20602_gyro_z, 6);
 
-        tft180_show_float(78, 0, icm20602_acc_transition(icm20602_acc_x), 2, 2);
-        tft180_show_float(78, 16, icm20602_acc_transition(icm20602_acc_y), 2, 2);
-        tft180_show_float(78, 32, icm20602_acc_transition(icm20602_acc_z), 2, 2);
-        tft180_show_float(78, 48, icm20602_gyro_transition(icm20602_gyro_x), 2, 2);
-        tft180_show_float(78, 64, icm20602_gyro_transition(icm20602_gyro_y), 2, 2);
-        tft180_show_float(78, 80, icm20602_gyro_transition(icm20602_gyro_z), 2, 2);
+        // tft180_show_float(78, 0, icm20602_acc_transition(icm20602_acc_x), 2, 2);
+        // tft180_show_float(78, 16, icm20602_acc_transition(icm20602_acc_y), 2, 2);
+        // tft180_show_float(78, 32, icm20602_acc_transition(icm20602_acc_z), 2, 2);
+        // tft180_show_float(78, 48, icm20602_gyro_transition(icm20602_gyro_x), 2, 2);
+        // tft180_show_float(78, 64, icm20602_gyro_transition(icm20602_gyro_y), 2, 2);
+        // tft180_show_float(78, 80, icm20602_gyro_transition(icm20602_gyro_z), 2, 2);
 
         
-        attitude_solution_func(icm20602_acc_x, icm20602_acc_y, icm20602_acc_z, icm20602_gyro_x, icm20602_gyro_y, icm20602_gyro_z, &yaw, &rol, &pitch);
+        // attitude_solution_func(icm20602_acc_x, icm20602_acc_y, icm20602_acc_z, icm20602_gyro_x, icm20602_gyro_y, icm20602_gyro_z, &yaw, &rol, &pitch);
         
 
 
-        tft180_show_string(0, 96, "yaw");
-        tft180_show_string(0, 112, "rol");
-        tft180_show_string(0, 128, "pitch");
+        // tft180_show_string(0, 96, "yaw");
+        // tft180_show_string(0, 112, "rol");
+        // tft180_show_string(0, 128, "pitch");
 //        tft180_show_int(42, 96, yaw, 3);
 //        tft180_show_int(42, 112, rol, 3);
 //        tft180_show_int(42, 128, pitch, 3);
@@ -216,27 +217,27 @@ int core0_main(void)
         */
 
         /* 方案二:在方案一的基础上,识别+360到+0之间的突变 */
-        yaw = (yaw < 0) ? (yaw + 360) : yaw;
-        rol = (rol < 0) ? (rol + 360) : rol;
-        pitch = (pitch < 0) ? (pitch + 360) : pitch;
-        tft180_show_int(42, 96, yaw, 3);
-        tft180_show_int(42, 112, rol, 3);
-        tft180_show_int(42, 128, pitch, 3);
-        if(absValue(yaw - yawLast) >= 180){ 
-            yawCount -= signValue(yaw - yawLast); 
-        }
-        if(absValue(rol - rolLast) >= 180){ 
-            rolCount -= signValue(rol - rolLast); 
-        }
-        if(absValue(pitch - pitchLast) >= 180){ 
-            pitchCount -= signValue(pitch - pitchLast);
-        }
-        yawLast = yaw;
-        rolLast = rol;
-        pitchLast = pitch;
-        tft180_show_int(84, 96, yaw + yawCount * 360, 4);
-        tft180_show_int(84, 112, rol + rolCount * 360, 4);
-        tft180_show_int(84, 128, pitch + pitchCount * 360, 4);
+        // yaw = (yaw < 0) ? (yaw + 360) : yaw;
+        // rol = (rol < 0) ? (rol + 360) : rol;
+        // pitch = (pitch < 0) ? (pitch + 360) : pitch;
+        // tft180_show_int(42, 96, yaw, 3);
+        // tft180_show_int(42, 112, rol, 3);
+        // tft180_show_int(42, 128, pitch, 3);
+        // if(absValue(yaw - yawLast) >= 180){ 
+        //     yawCount -= signValue(yaw - yawLast); 
+        // }
+        // if(absValue(rol - rolLast) >= 180){ 
+        //     rolCount -= signValue(rol - rolLast); 
+        // }
+        // if(absValue(pitch - pitchLast) >= 180){ 
+        //     pitchCount -= signValue(pitch - pitchLast);
+        // }
+        // yawLast = yaw;
+        // rolLast = rol;
+        // pitchLast = pitch;
+        // tft180_show_int(84, 96, yaw + yawCount * 360, 4);
+        // tft180_show_int(84, 112, rol + rolCount * 360, 4);
+        // tft180_show_int(84, 128, pitch + pitchCount * 360, 4);
 
         // 此处编写需要循环执行的代码
     }
