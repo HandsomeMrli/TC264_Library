f4a104c59a13150057300737e2b0caca074cadc7
Yaner <1468275133@qq.com>
2023-04-03 19:51:43 +0800

    完善PID接口

--- Seekfree_TC264_Opensource_Library/code/motor.c
+++ Seekfree_TC264_Opensource_Library/code/motor.c
@@ -5,22 +5,26 @@ PIDValue velPIDy, velPIDl, velPIDr;
 PIDValue angPIDx, angPIDy, angPIDz;
 PIDValue angVelPIDx, angVelPIDy, angVelPIDz;
 
-void updatexxx(){
-    
+void updateMotors(
+        int16 motorLeftSpeed, int16 motorRightSpeed, int16 motorBottomSpeed, 
+        int32 cameraSpeedTarget, int32 cameraTurnTarget, 
+        int32 pitchX, int32 rollY, int32 yawZ,
+        int32 angVelX, int32 angVelY, int32 angVelZ){
+
     // 速度环更新
-    velPIDl.target = 0; velPIDl.measurement = 左动量轮转速; __updatePID(&velPIDl);
-    velPIDr.target = 0; velPIDr.measurement = 右动量轮转速; __updatePID(&velPIDr);
-    velPIDy.target = 摄像头目标值; velPIDy.measurement = 底轮转速; __updatePID(&velPIDy);
+    velPIDl.target = 0; velPIDl.measurement = motorLeftSpeed; __updatePID(&velPIDl);
+    velPIDr.target = 0; velPIDr.measurement = motorRightSpeed; __updatePID(&velPIDr);
+    velPIDy.target = cameraSpeedTarget; velPIDy.measurement = motorBottomSpeed; __updatePID(&velPIDy);
     
     // 角度环更新
-    angPIDx.target = velPIDl.deltaOutput + velPIDr.deltaOutput; angPIDx.measurement = 陀螺仪欧拉角x; __updatePID(&angPIDx); // TODO:左右两轮的deltaOutput是相加还是相减?
-    angPIDy.target = velPIDy.deltaOutput; angPIDy.measurement = 陀螺仪欧拉角y; __updatePID(&angPIDy);
-    angPIDz.target = 摄像头目标值;          angPIDz.measurement = 陀螺仪欧拉角z; __updatePID(&angPIDz);
+    angPIDx.target = velPIDl.deltaOutput + velPIDr.deltaOutput; angPIDx.measurement = pitchX; __updatePID(&angPIDx); // TODO:左右两轮的deltaOutput是相加还是相减?
+    angPIDy.target = velPIDy.deltaOutput; angPIDy.measurement = rollY; __updatePID(&angPIDy);
+    angPIDz.target = cameraTurnTarget;    angPIDz.measurement = yawZ; __updatePID(&angPIDz);
 
     // 角速度环更新
-    angVelPIDx.target = angPIDx.deltaOutput; angVelPIDx.measurement = icm20602_gyro_x(陀螺仪角速度x); __updatePID(&angVelPIDx);   
-    angVelPIDy.target = angPIDy.deltaOutput; angVelPIDy.measurement = icm20602_gyro_y(陀螺仪角速度y); __updatePID(&angVelPIDy);   
-    angVelPIDz.target = angPIDz.deltaOutput; angVelPIDz.measurement = icm20602_gyro_z(陀螺仪角速度z); __updatePID(&angVelPIDz);   
+    angVelPIDx.target = angPIDx.deltaOutput; angVelPIDx.measurement = angVelX; __updatePID(&angVelPIDx);   
+    angVelPIDy.target = angPIDy.deltaOutput; angVelPIDy.measurement = angVelY; __updatePID(&angVelPIDy);   
+    angVelPIDz.target = angPIDz.deltaOutput; angVelPIDz.measurement = angVelZ; __updatePID(&angVelPIDz);   
 
     // PWM更新
     setMotor(&motorLeft, ASSIGN, angVelPIDx.deltaOutput + angVelPIDz.deltaOutput); // TODO:左右两轮的deltaOutput是相加还是相减?
